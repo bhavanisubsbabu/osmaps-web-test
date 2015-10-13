e4161f53e1a271a40d28ef446a0724d58b1d354a
unknown <RKKunaparaju@ND28823.ordsvy.gov.uk>
2015-10-13 15:37:08 +0100

    minor changes for nightly builds

--- html5-automationtest/src/test/java/uk/co/ordnancesurvey/accceptanceTest/RegressionTest.java
+++ html5-automationtest/src/test/java/uk/co/ordnancesurvey/accceptanceTest/RegressionTest.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		format = {"pretty","html:target/reports/reg-test-report","json:target/reports/reg-test-report/report.json"}, 
-		features = {"src/test/resources/features.functional/"},glue="uk.co.ordnancesurvey.stepdefs",tags={"@wip"})
+		features = {"src/test/resources/features.functional/"},glue="uk.co.ordnancesurvey.stepdefs",tags={"@regression","~@wip"})
 public class RegressionTest {
 
 }
